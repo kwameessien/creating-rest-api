ba31a2e0d7400c2c8c1d411041e2a668b5695927
kwameessien <180320488+kwameessien@users.noreply.github.com>
2026-02-16 11:49:54 -0500

    Created two async functions: updateBook and deleteBook in books.js in frontend folder

--- starting-code/frontend/src/api/books.js
+++ starting-code/frontend/src/api/books.js
@@ -29,5 +29,24 @@ export const getBooks = async () => {
 };
 
 // TODO: Create the updateBook function that takes the arguments id, newTitle, newStart, newEnd. Inside of the function, create a PUT request for the specified book to be updated. Return the status of the response at the end of the function.
+export const updateBook = async (id, newTitle, newStart, newEnd) => {
+  const response = await fetch(`${API_ENDPOINT}/books/${id}`, {
+    method: "PUT",
+    body: JSON.stringify({
+      title: newTitle,
+      start: newStart,
+      end: newEnd
+    }),
+  });
+
+  return response.status;
+};
 
 // TODO: Create the deleteBook function that takes the id of the book to be deleted as an argument. Inside of the function, create a DELETE request for the specified book to be deleted. Return the status of the response at the end of the function.
+export const deleteBook = async (id) => {
+  const response = await fetch(`${API_ENDPOINT}/books/${id}`, {
+    method: "DELETE",
+  });
+
+  return response.status;
+};
